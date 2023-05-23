743530066979a36c09ef193d27072948f56f0910
Philippe GLESSMER <glessmer@live.fr>
2023-05-23 22:39:21 +0200

    Group Validation

--- src/Entity/Job.php
+++ src/Entity/Job.php
@@ -2,10 +2,22 @@
 
 namespace App\Entity;
 
-class Job
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\GroupSequence;
+use Symfony\Component\Validator\GroupSequenceProviderInterface;
+
+/**
+* @Assert\GroupSequenceProvider()
+ */
+class Job implements GroupSequenceProviderInterface
 {
     private $title;
     private $place;
+    private $contact;
+    /**
+     * @Assert\IsTrue(groups={"ForeignJob"},message="New-Work necessite un permis de travail!")
+     */
+    private $autorizationWork;
     /**
      * @return mixed
      */
@@ -37,5 +49,44 @@ class Job
     {
         $this->place = $place;
     }
+    /**
+     * @return mixed
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+    /**
+     * @param mixed $contact
+     */
+    public function setContact($contact): void
+    {
+        $this->contact = $contact;
+    }
 
+    /**
+     * @return mixed
+     */
+    public function getAutorizationWork()
+    {
+        return $this->autorizationWork;
+    }
+
+    /**
+     * @param mixed $autorizationWork
+     */
+    public function setAutorizationWork($autorizationWork): void
+    {
+        $this->autorizationWork = $autorizationWork;
+    }
+
+
+    public function getGroupSequence(): array|GroupSequence
+    {
+        $group = ['Job'];
+        if($this->getPlace() == 'New-York'){
+            $group[] = "ForeignJob";
+        }
+        return $group;
+    }
 }
--- src/Form/JobType.php
+++ src/Form/JobType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Service\RandomPlace;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\{ChoiceType, SubmitType, TextType};
+use Symfony\Component\Form\Extension\Core\Type\{CheckboxType, ChoiceType, EmailType, SubmitType, TextType};
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
 
 class JobType extends AbstractType
 {
@@ -16,11 +18,20 @@ class JobType extends AbstractType
         $builder
             ->add('title', TextType::class, [
                 'attr' => [
+                    'class' => 'form-control mb-3',
                     'placeholder' => $options['my_title']
-                ]
+                ],
+                'constraints' => new Length(['min' => 3, 'minMessage' => 'Minimum 3 lettres'])
             ])
-            ->add('place', PlaceType::class,[
-
+            ->add('place', PlaceType::class)
+            ->add('contact', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ],
+                'constraints' => new Email(),
+            ])
+            ->add('autorizationWork', CheckboxType::class, [
+                'required' => false,
             ])
             ->add('save', SubmitType::class)
         ;
@@ -30,6 +41,7 @@ class JobType extends AbstractType
     {
         $resolver->setDefaults([
             // Configure your form options here
+            "validation_groups" => false,
 
         ]);
         $resolver->setRequired([
