d448841e401bbffe504753d37d01c88a8ac526d0
Philippe GLESSMER <glessmer@live.fr>
2023-05-23 20:15:55 +0200

    Form Controller Bourin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Job;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,10 +20,26 @@ class HomeController extends AbstractController
         ]);
     }
     #[Route('/add', name: 'app_home_add')]
-    public function add(): Response
+    public function add(Request $request): Response
     {
+    $job = new Job();
+        $form = $this->createFormBuilder($job)
+            ->add('title', TextType::class)
+            ->add('place', TextType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //dd($_POST['form']['title']);
+            //dd($request->request->get('form')['title'];
+            //dd($request->get('form')['title'];
+//            dd($form->getData());
+            dd($job);
+        }
         return $this->render('home/add.html.twig', [
             'controller_name' => 'HomeController',
+            'form' => $form->createView(),
         ]);
     }
 }
